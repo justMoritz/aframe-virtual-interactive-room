158beb411165ba7bd2d305c851f4c6db166b1551
mmcmoritz <moritz@mailmmc.com>
2020-03-06 16:30:02 -0600

    added new images

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 
       <!-- All Images -->
       <a-assets>
-        <img id="img_sky" src="src/Villas-on-Rio_B6_360_2.jpg?v=<?=$globalVersion?>">
+        <img id="img_sky" src="src/B6_360_1.jpg?v=<?=$globalVersion?>">
         <img id="img_circle0" src="src/circle0.png?v=<?=$globalVersion?>">
         <img id="img_circle1" src="src/circle1.png?v=<?=$globalVersion?>">
         <img id="img_circle2" src="src/circle2.png?v=<?=$globalVersion?>">
